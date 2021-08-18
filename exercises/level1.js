7bbd9ecd6b71f109dfbc6fc7cc77be96a0e55ae1
Nick Kulway <nkulway@gmail.com>
2021-08-17 21:44:55 -0400

    First Commit

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,40 @@
 function lvl1exercise1 () {
-  // Declare a variable without instantiating it and return it!
-
+let newVariable;
+return newVariable;
 }
 
 function lvl1exercise2 () {
-  // Declare and instantiate a number and return it
-
+let num = 1;
+return num;
 }
 
 function lvl1exercise3 () {
-  // Declare and instantiate a floating point number that is not a whole number and return it
-
+let float = 1.5;
+return float;
 }
 
 function lvl1exercise4 () {
-  // Declare and instantiate a string "Hello World!" and return it
-
+let string = 'Hello World!';
+return string;
 }
 
 function lvl1exercise5 () {
-  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+let num = 4;
+let string = "Hello World!";
+const arr = [string, num];
+return arr;
 }
 
 function lvl1exercise6 () {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+let object = {
+  key1:"Hello World!",
+  key2: 4
+}
+return object;
 }
 
 function lvl1exercise7 () {
-  // Declare and instantiate a boolean value 'false' and return it
-
+let isEven = false;
+return isEven;
 }
--- exercises/level3.js
+++ exercises/level3.js
@@ -1,17 +1,25 @@
 function lvl3exercise1 () {
   // Create a "hello" and a "world", return the concatenation of the two
+  let str1 = 'hello';
+  let str2 = 'world';
+  return str1 + str2;
 
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+let str1 = 'hello';
+let num = 12;
+return str1 + num;
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+let num = 12;
+let word = 'dogs';
+return num + word;
 }
+
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
